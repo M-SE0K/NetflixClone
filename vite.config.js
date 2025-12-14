86178cc28b50f6f709542d74cb10556de7225f65
M-SE0K <seg082911@gmail.com>
2025-12-15 00:04:27 +0900

    [build] 배포 경로 수정

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/WSD-Assignment-02/',
+  // GitHub Pages 배포 경로: https://m-se0k.github.io/NetflixClone/
+  base: '/NetflixClone/',
   plugins: [react()],
 })
